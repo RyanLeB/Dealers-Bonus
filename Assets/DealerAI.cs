eccd6d8b87a90a2da048888894c6396eb06a91a3
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2024-09-10 10:11:47 -0300

    Testing for Rider
    Testing for Rider Editor

---
+++ Assets/DealerAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealerAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting Dealer AI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
