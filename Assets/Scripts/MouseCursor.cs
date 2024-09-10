29314d781620caddeab5f29d3090789286329b28
Ryan LeBlanc <122310498+RyanLeB@users.noreply.github.com>
2024-09-10 11:20:49 -0300

    Mouse Cursor implemented

---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+
+    private Vector2 cursorHotSpot;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorHotSpot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
+        Cursor.SetCursor(cursorTexture, cursorHotSpot, CursorMode.Auto);
+    }
+
+}
